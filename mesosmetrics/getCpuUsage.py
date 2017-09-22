ce29103ceff1abec76322eeb3ac65cae300cc30f
David Jennings <djennings@esri.com>
2017-09-22 12:14:29 -0500

    Create getCpuUsage.py

---
+++ mesosmetrics/getCpuUsage.py
@@ -0,0 +1,50 @@
+import urllib
+import json
+import time
+
+url = "http://localhost:5051/monitor/statistics"
+
+
+executors = {}
+
+
+response = urllib.urlopen(url)
+
+data = json.loads(response.read())
+
+for itm in data:
+    executor = {}
+    id = itm["executor_id"]
+    executor["name"] = itm["executor_name"]
+
+    a = {}
+    a["cpu_system"] = itm["statistics"]["cpus_system_time_secs"]
+    a["cpu_user"] = itm["statistics"]["cpus_user_time_secs"]
+    a["ts"] = itm["statistics"]["timestamp"]
+    executor["a"] = a
+
+    executors[id] = executor
+
+time.sleep(5)
+response = urllib.urlopen(url)
+
+data = json.loads(response.read())
+
+    
+for itm in data:
+    id = itm["executor_id"]
+    
+    b = {}
+    b["cpu_system"] = itm["statistics"]["cpus_system_time_secs"]
+    b["cpu_user"] = itm["statistics"]["cpus_user_time_secs"]
+    b["ts"] = itm["statistics"]["timestamp"]
+    
+
+    executors[id]["b"] = b
+    
+
+for id,itm in executors.items():
+    cpus_total_usage = ((itm["b"]["cpu_system"]-itm["a"]["cpu_system"]) + \
+                        (itm["b"]["cpu_user"]-itm["a"]["cpu_user"])) / \
+                        (itm["b"]["ts"]-itm["a"]["ts"])
+    print(str(id) + " : " + str(cpus_total_usage))
