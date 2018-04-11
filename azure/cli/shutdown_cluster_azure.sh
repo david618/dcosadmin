6cc350915cee25f63fb137a15139fe7fd0fa2035
David Jennings <djennings@esri.com>
2018-04-11 15:34:10 -0500

    Add files via upload

---
+++ azure/cli/shutdown_cluster_azure.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RG="dj06"
+ACTION="deallocate"
+NUM_MASTERS=1
+NUM_AGENTS=6
+NUM_AGENTS_PUBLIC=1
+
+
+
+for num in $(seq 1 ${NUM_MASTERS}); do
+  echo "az vm ${ACTION} --resource-group ${RG} --name ${RG}m${num}"
+  az vm ${ACTION} --resource-group ${RG} --name ${RG}m${num}
+done
+
+for num in $(seq 1 ${NUM_AGENTS_PUBLIC}); do
+  echo "az vm ${ACTION} --resource-group ${RG} --name ${RG}p${num}"
+  az vm ${ACTION} --resource-group ${RG} --name ${RG}p${num}
+done
+
+for num in $(seq 1 ${NUM_AGENTS}); do
+  echo "az vm ${ACTION} --resource-group ${RG} --name ${RG}a${num}"
+  az vm ${ACTION} --resource-group ${RG} --name ${RG}a${num}
+done
+
